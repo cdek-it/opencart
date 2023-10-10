9d74c311c741a0f6438072ebc4e888bc5641f73d
Klem65 <ilyaklem65@gmail.com>
2023-10-10 07:07:37 +0300

    Исправление проблемы добавления новых полей в таблицу заказа

--- src/upload/system/library/cdek_official/CdekOrderMetaRepository.php
+++ src/upload/system/library/cdek_official/CdekOrderMetaRepository.php
@@ -4,8 +4,11 @@ class CdekOrderMetaRepository
 {
     public static function create($db, $prefix)
     {
-        $db->query("
-            CREATE TABLE IF NOT EXISTS `" . $prefix . "cdek_order_meta` (
+        $tableExists = $db->query("SHOW TABLES LIKE '" . $prefix . "cdek_order_meta'")->num_rows > 0;
+
+        if (!$tableExists) {
+            $createTableSQL = "
+            CREATE TABLE `" . $prefix . "cdek_order_meta` (
                 `id` INT(11) NOT NULL AUTO_INCREMENT,
                 `order_id` INT(11) NOT NULL,
                 `cdek_number` VARCHAR(255) NOT NULL,
@@ -16,11 +19,28 @@ class CdekOrderMetaRepository
                 `to_location` VARCHAR(255) NOT NULL,
                 `pvz_code` VARCHAR(255) NOT NULL,
                 `created` INT(1) DEFAULT 0,
+                `deleted` INT(1) DEFAULT 0,
                 PRIMARY KEY (`id`),
                 UNIQUE KEY `order_id_unique` (`order_id`),
                 FOREIGN KEY (`order_id`) REFERENCES `" . $prefix . "order`(`order_id`)
             ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-        ");
+        ";
+            $db->query($createTableSQL);
+        }
+
+        $existingColumns = array_map(fn($el) => $el['Field'], $db->query("SHOW COLUMNS FROM `" . $prefix . "cdek_order_meta`")->rows);
+
+        $columnsToAdd = [
+            'deleted' => 'deleted INT(1) DEFAULT 0',
+        ];
+
+        $columnsToAddSQL = array_diff(array_keys($columnsToAdd), $existingColumns);
+        $columnsForAdd = array_map(fn($em) => $columnsToAdd[$em], $columnsToAddSQL);
+
+        if (!empty($columnsToAddSQL)) {
+            $alterTableSQL = "ALTER TABLE `" . $prefix . "cdek_order_meta` ADD " . implode(", ADD ", $columnsForAdd);
+            $db->query($alterTableSQL);
+        }
     }
 
     public static function insertPvzCode($db, $prefix, $orderId, $pvzCode)
